f831d0b6002d6d7050955a825b3eaaeb97ad827e
fbirk <fabianbirk.aar@t-online.de>
2019-04-16 20:07:51 +0200

    Minor changes

--- script.js
+++ script.js
@@ -1,39 +1,19 @@
-/* script by https://wiki.selfhtml.org/wiki/JavaScript/Tutorials/Web_Animations/Animationen_synchronisieren#mehrere_Animationen_zeitversetzt_starten */
+var slideIndex = 0;
+showSlides();
 
-'use strict';
-document.addEventListener('DOMContentLoaded', function () {
-	// Auswahl des Bildwechslers und seiner Kindelemente
-	var changer = document.querySelector('#gallery'),
-		children = changer.querySelectorAll('figure'),
-		intervall = 7000; // Dauer eines Bildes
-	// sets container, puts figure-elements horizontal 
-	changer.classList.add('changer');
-	var keyframes = [
-		{
-			opacity: 0
-		},
-		{
-			opacity: 1,
-			offset: 1 / (children.length - 2) * 1 / 3
-		},
-		{
-			opacity: 1,
-			offset: 1 / (children.length - 2) * 2 / 3
-		},
-		{
-			opacity: 0,
-			offset: 1 / (children.length - 2)
-		},
-		{
-			opacity: 0
-		}
-  ];
-	// alle figure-Elemente werden in einer Schleife animiert  
-	for (var index = 0; index < children.length; index++) {
-		children[index].animate(keyframes, {
-			duration: intervall * children.length,
-			delay: index * intervall,
-			iterations: Infinity
-		});
-	}
-});
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("crop");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
