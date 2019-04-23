b0f28b7eff49959fb1617a49bd6c3ddf07a37dc6
fbirk <fabianbirk.aar@t-online.de>
2019-04-23 20:47:23 +0200

    further improvements

--- script.js
+++ script.js
@@ -4,16 +4,17 @@ showSlides();
 function showSlides() {
   var i;
   var slides = document.getElementsByClassName("crop");
-  var dots = document.getElementsByClassName("dot");
+
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
+
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+
+  if (slideIndex > slides.length) {
+    slideIndex = 1
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+
+  slides[slideIndex - 1].style.display = "block";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
